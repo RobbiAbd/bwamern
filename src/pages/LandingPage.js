3c57783ba0bfb8841c9ded3624292d2baa354cd0
robbiabd <robbyabd10@gmail.com>
2021-04-02 10:34:24 +0700

    assets

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,12 +1,15 @@
 import { React, Component } from "react";
 
 import Header from "parts/Header";
+import landingPageJson from "json/landingPage";
+import Hero from "parts/Hero";
 
 export default class LandingPage extends Component {
   render() {
     return (
       <>
         <Header {...this.props}></Header>
+        <Hero data={landingPageJson.hero}></Hero>
       </>
     );
   }
