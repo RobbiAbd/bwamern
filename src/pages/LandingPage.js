2d520fc5d8cd73480c626540527fc9b0397155bd
robbiabd <robbyabd10@gmail.com>
2021-05-17 22:16:04 +0700

    categories

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -4,6 +4,7 @@ import Header from "parts/Header";
 import landingPageJson from "json/landingPage";
 import Hero from "parts/Hero";
 import MostPicked from "parts/MostPicked";
+import Categories from "parts/Categories";
 
 export default class LandingPage extends Component {
 	constructor(props) {
@@ -17,6 +18,7 @@ export default class LandingPage extends Component {
 				<Header {...this.props}></Header>
 				<Hero refMostPicked={this.refMostPicked} data={landingPageJson.hero}></Hero>
 				<MostPicked refMostPicked={this.refMostPicked} data={landingPageJson.mostPicked}></MostPicked>
+				<Categories data={landingPageJson.categories}></Categories>
 			</>
 		);
 	}
